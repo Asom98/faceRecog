30295874e3a6a985d559c333b62413198c970e95
Amer Harb <amer.harb@gmail.com>
2024-10-13 08:36:42 +0200

    format

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,41 +2,43 @@
 
 import numpy as np
 
+
+def softmax(z):
+    exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
+    return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+
 class LogisticRegression:
     def __init__(self, input_size, num_classes, learning_rate=0.1, regularization=0.001):
-        self.W = np.random.randn(input_size, num_classes) * 0.01
+        self.w = np.random.randn(input_size, num_classes) * 0.01
         self.b = np.zeros((1, num_classes))
         self.learning_rate = learning_rate
         self.regularization = regularization
-    
-    def softmax(self, z):
-        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
-        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
-    
-    def compute_loss(self, y, y_hat):
+
+    def _compute_loss(self, y, y_hat):
         m = y.shape[0]
         cross_entropy_loss = -np.sum(y * np.log(y_hat + 1e-8)) / m
-        l2_loss = self.regularization * np.sum(np.square(self.W)) / 2
+        l2_loss = self.regularization * np.sum(np.square(self.w)) / 2
         return cross_entropy_loss + l2_loss
     
-    def compute_gradients(self, X, y, y_hat):
-        m = X.shape[0]
-        dW = np.dot(X.T, (y_hat - y)) / m + self.regularization * self.W
+    def _compute_gradients(self, x, y, y_hat):
+        m = x.shape[0]
+        d_w = np.dot(x.T, (y_hat - y)) / m + self.regularization * self.w
         db = np.sum(y_hat - y, axis=0, keepdims=True) / m
-        return dW, db
+        return d_w, db
     
-    def train(self, X, y, epochs=1000):
+    def train(self, x, y, epochs=1000):
         for i in range(epochs):
-            z = np.dot(X, self.W) + self.b
-            y_hat = self.softmax(z)
-            loss = self.compute_loss(y, y_hat)
-            dW, db = self.compute_gradients(X, y, y_hat)
-            self.W -= self.learning_rate * dW
+            z = np.dot(x, self.w) + self.b
+            y_hat = softmax(z)
+            loss = self._compute_loss(y, y_hat)
+            d_w, db = self._compute_gradients(x, y, y_hat)
+            self.w -= self.learning_rate * d_w
             self.b -= self.learning_rate * db
             if i % 100 == 0:
                 print(f"Epoch {i}, Loss: {loss}")
     
-    def predict(self, X):
-        z = np.dot(X, self.W) + self.b
-        y_hat = self.softmax(z)
+    def predict(self, x):
+        z = np.dot(x, self.w) + self.b
+        y_hat = softmax(z)
         return np.argmax(y_hat, axis=1)
